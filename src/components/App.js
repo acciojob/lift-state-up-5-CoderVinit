5ec5bb0a6bac7aad6ab27ff9b02e6b27e15b7fcc
VINIT KUMAR PATEL <101181057+CoderVinit@users.noreply.github.com>
2025-02-28 09:15:49 +0530

    changes made in parent comp

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,20 @@
-
-import React from "react";
-import './../styles/App.css';
-import Parent from "./ParentComponent";
+import React, { useState } from "react";
+import "./../styles/App.css";
+import Child from "./ChildComponent";
 
 const App = () => {
-  
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div>
-        {/* Do not remove the main div */}
-        <Parent/>
+      {/* Do not remove the main div */}
+      <h1>Parent Component</h1>
+      {isLoggedIn ? (
+        <h2>You are logged in!</h2>
+      ) : (
+        <Child setIsLoggedIn={setIsLoggedIn} />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
